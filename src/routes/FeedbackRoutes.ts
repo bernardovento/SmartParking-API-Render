0b7878f39e600701b0cbaebb7d3cab7b91117f32
Bernardo Venturotti Braun Rauta Ramos <40549558+bernardovento@users.noreply.github.com>
2024-09-23 23:29:30 -0300

    Update FeedbackRoutes.ts

--- src/routes/FeedbackRoutes.ts
+++ src/routes/FeedbackRoutes.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
-
 import FeedbackController from "../controllers/FeedbackController";
 
-const FeedbackRouter = Router()
+const FeedbackRouter = Router();
 
-//Listar usuários
+// Listar todos os feedbacks
+FeedbackRouter.get("/feedbacks/all", FeedbackController.listAllFeedback);
+
+// Listar feedbacks válidos (validContent e validName como 'valido')
 FeedbackRouter.get("/feedbacks", FeedbackController.listFeedback);
 
-//Inserir usuários
+// Inserir um novo feedback
 FeedbackRouter.post("/feedback", FeedbackController.createFeedback);
 
+// Deletar um feedback pelo ID
 FeedbackRouter.delete("/feedback/:id", FeedbackController.deleteFeedback);
-
 
 export default FeedbackRouter;
